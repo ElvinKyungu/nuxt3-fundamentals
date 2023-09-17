63f96901c82b51fbccbb26245ad9143d0890ef88
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-17 13:42:00 +0200

    add : installing supabase for the backend side

--- pages/Movies/[id].vue
+++ pages/Movies/[id].vue
@@ -4,6 +4,8 @@
 
 <script setup>
     const route = useRoute();
+    console.log(route.params);
     const message = computed(() => `user id ${route.params.id}`);
-    
+    const fu = JSON.stringify(message)
+    console.log(fu);
 </script>
