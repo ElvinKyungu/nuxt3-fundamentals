3395c529ec8d9e7c725038eff0615477a5f7e7d5
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-19 13:20:56 +0200

    feat : adding the confirm redirect page with logic

--- pages/login.vue
+++ pages/login.vue
@@ -10,7 +10,7 @@
                     <h1 class="text-xl text-center font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                         Connectez-vous
                     </h1>
-                    <form class="space-y-4 md:space-y-6" @submit.prevent="loginUser()">
+                    <form class="space-y-4 md:space-y-6" @submit.prevent="loginUser">
                         <div>
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre adresse email</label>
                             <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-indigo-500 focus:border-indigo-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@example.com" required="">
@@ -49,9 +49,11 @@ const SuccessMsg = ref(null);
 
 const loginUser = async () => {
     try {
-        const { data, error } = await supabase.auth.signInWithPassword({
+        const { data, error } = await supabase.auth.signInWithOtp({
             email: email.value,
-            password: password.value
+            options: {
+                emailRedirectTo: 'http://localhost:3000/confirm',
+            }
         });
         if (error) {
           throw new Error(error.message);
@@ -61,15 +63,6 @@ const loginUser = async () => {
         console.error('Erreur lors de la connexion:', error.message);
     }
 }
-    // console.log("Hello");
-    // const { error } = await supabase.auth.signInWithOtp({
-    //     email: email.value,
-    //     options: {
-    //         emailRedirectTo: 'http://localhost:3000/confirm',
-    //     }
-    // })
-    // if (error) console.log(error)
-
 </script>
 <style>
 </style>
