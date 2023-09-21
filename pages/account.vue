6977498d399ce96372368c951f1bdd5320a2d2e1
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-21 11:43:03 +0200

    👌 adding account page and same functionality

---
+++ pages/account.vue
@@ -0,0 +1,69 @@
+<script setup>
+const supabase = useSupabaseClient()
+
+const loading = ref(true)
+const username = ref('')
+const website = ref('')
+const avatar_path = ref('')
+
+loading.value = true
+const user = useSupabaseUser()
+
+let { data } = await supabase
+  .from('profile')
+  .select(`display_name, email`)
+  .eq('id', user.value.id)
+  .single()
+
+if (data) {
+    display_name.value = data.username
+    email.value = data.website
+    console.log(data);
+}
+
+loading.value = false
+
+
+async function signOut() {
+  try {
+    loading.value = true
+    let { error } = await supabase.auth.signOut()
+    if (error) throw error
+    user.value = null
+  } catch (error) {
+    alert(error.message)
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <form class="form-widget" @submit.prevent="updateProfile">
+    <div>
+      <label for="email">Email</label>
+      <input id="email" type="text" :value="user.email" disabled />
+    </div>
+    <div>
+      <label for="username">Username</label>
+      <input id="username" type="text" v-model="username" />
+    </div>
+    <div>
+      <label for="website">Website</label>
+      <input id="website" type="url" v-model="website" />
+    </div>
+
+    <div>
+      <input
+        type="submit"
+        class="button primary block"
+        :value="loading ? 'Loading ...' : 'Update'"
+        :disabled="loading"
+      />
+    </div>
+
+    <div>
+      <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
+    </div>
+  </form>
+</template>
