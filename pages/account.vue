3ed29adb65ff3ead1524a52afef19c558d9b3356
ElvinKyungu <elvinkyungu.75@gmail.com>
2023-09-21 12:05:00 +0200

    Fix : Fixing user function and removing formaular side

--- pages/account.vue
+++ pages/account.vue
@@ -4,7 +4,6 @@ const supabase = useSupabaseClient()
 const loading = ref(true)
 const username = ref('')
 const email = ref('')
-const avatar_path = ref('')
 
 loading.value = true
 const user = useSupabaseUser()
@@ -37,31 +36,7 @@ async function signOut() {
 </script>
 
 <template>
-  <form class="form-widget" @submit.prevent="updateProfile">
-    <div>
-      <label for="email">Email</label>
-      <input id="email" type="text" :value="user.email" disabled />
-    </div>
-    <div>
-      <label for="username">Username</label>
-      <input id="username" type="text" v-model="username" />
-    </div>
-    <div>
-      <label for="website">Website</label>
-      <input id="website" type="url" v-model="website" />
-    </div>
-
-    <div>
-      <input
-        type="submit"
-        class="button primary block"
-        :value="loading ? 'Loading ...' : 'Update'"
-        :disabled="loading"
-      />
-    </div>
-
     <div>
       <button class="button block" @click="signOut" :disabled="loading">Sign Out</button>
     </div>
-  </form>
 </template>
